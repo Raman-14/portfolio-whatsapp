31503c94d336948058de4c63ab6bc48b2df348b7
Raman-14 <ramanofficial014@gmail.com>
2023-05-26 00:18:11 +0530

    add download resume

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  @ViewChild('scrollToElement', { static: false })
+  scrollToElement!: ElementRef;
 
+  message = 'hide-msg';
+
+  handleClick(){
+    this.message = 'show-msg';
+   this.scrollElementIntoView(); 
+  }
+  private scrollElementIntoView() {
+    const element = this.scrollToElement.nativeElement;
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 }
