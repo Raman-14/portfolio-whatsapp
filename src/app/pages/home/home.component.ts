87c001a76982bc08b1015ec6e72a571b169e3e7a
Raman-14 <ramanofficial014@gmail.com>
2023-06-04 18:50:45 +0530

    add more changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,21 +6,29 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(private renderer: Renderer2) {}
+  // constructor(private renderer: Renderer2) {}
   
 
   message = 'hide-msg';
 
   handleClick(){
+    
     this.message = 'show-msg';
     setTimeout(() => {
-      this.scrollElementIntoView();
-    }, 0);
+      this.message = 'hide-msg';
+    }, 10000); // 10 seconds (10000 milliseconds)
   }
-  private scrollElementIntoView() {
-    const element = document.getElementById('body-row-4'); // Replace with the actual ID of the element you want to scroll to
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-  }
+
+  // handleClick(){
+  //   this.message = 'show-msg';
+  //   setTimeout(() => {
+  //     this.scrollElementIntoView();
+  //   }, 0);
+  // }
+  // private scrollElementIntoView() {
+  //   const element = document.getElementById('body-row-4'); // Replace with the actual ID of the element you want to scroll to
+  //   if (element) {
+  //     element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  //   }
+  // }
 }
