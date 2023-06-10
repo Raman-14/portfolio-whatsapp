e7aef8eeb4b51c29f356e2dc6b244712a9d7121f
Raman-14 <ramanofficial014@gmail.com>
2023-06-10 22:58:34 +0530

    add resume download button

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  message = 'hide-msg';
+
+  handleClick(){
+    window.open('https://drive.google.com/file/d/1rSHQ8yYE1ig9_ofbhyJCLxX8iIKAUEOK/view?usp=sharing', '_blank');
+    this.message = 'show-msg';
+    setTimeout(() => {
+      this.message = 'hide-msg';
+    }, 10000); // 10 seconds (10000 milliseconds)
+  }
 }
